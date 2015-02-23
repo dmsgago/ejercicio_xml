d30c9512a7ec0964b07b0db877228775f7d8b4c3
Diego Martín <dmsgago@gmail.com>
2015-02-23 18:06:22 +0100

    He añadido al fichero las lineas pertinentes para que el programa cargue el fichero xml y cuente el numero de museos en Sevilla

--- museos.py
+++ museos.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
 #Diego Martín Sánchez 1ºASIR, ejercicio_xml
+
+#Importa la clase etree de la librería lxml
+from lxml import etree
+
+#Almacena el contenido del fichero XML en una variable
+fichxml=etree.parse("museos.xml")
+
+#Muestra el numero de museos de la provincia de Sevilla
+len fichxml.getroot().findall("document/folder/placemark")
