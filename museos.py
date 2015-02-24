e2f3bf7cc07670f356e13d11571a9c6d97a96b35
Diego Martín <dmsgago@gmail.com>
2015-02-24 10:24:53 +0100

    He añadido las lineas necesarias al fichero para que muestre el nombre de todos los museos de Sevilla

--- museos.py
+++ museos.py
@@ -5,6 +5,13 @@
 #Importa la clase etree de la librería lxml
 from lxml import etree
 
+#Declara las variables utilizadas
+nsmap=''
+museos=[]
+nummuseos=0
+nombremuseo=''
+propiedad=[]
+
 #Almacena el contenido del fichero XML en una variable
 fich=etree.parse("museos.xml")
 #Almacena el nombre de espacio utilizado en el fichero xml
@@ -14,4 +21,13 @@ nsmap=raiz.tag[:-3]
 #Muestra el numero de museos de la provincia de Sevilla
 museos=fich.getroot().findall("%sDocument/%sFolder/%sPlacemark"%(nsmap,nsmap,nsmap))
 nummuseos=len(museos)
-print('Hay %d museos en Sevilla'%nummuseos)
+print('\nHay %d museos en Sevilla'%nummuseos)
+
+#Muestra el nombre de todos los museos
+print('\nLos nombres de los distintos museos son:')
+for museo in museos:
+    propiedades=museo.findall('%sExtendedData/%sSchemaData/%sSimpleData'%(nsmap,nsmap,nsmap))
+    for propiedad in propiedades:
+        if propiedad.attrib["name"] == "NOMBRE":
+            print('\t%s'%propiedad.text)
+
